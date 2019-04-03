520a5da10c9f85e15c452a5799a5be6ae6f52555
v_liwenyang <530788697@qq.com>
2019-04-03 18:10:58 +0800

    一对一增加

--- src/cn/edu360/mybatis/service/impl/OrdersServiceImpl.java
+++ src/cn/edu360/mybatis/service/impl/OrdersServiceImpl.java
@@ -2,10 +2,12 @@ package cn.edu360.mybatis.service.impl;
 
 import cn.edu360.mybatis.dao.ItemsDao;
 import cn.edu360.mybatis.dao.OrdersDao;
+import cn.edu360.mybatis.dao.UserDao;
 import cn.edu360.mybatis.model.OrdersModel;
 import cn.edu360.mybatis.service.OrdersService;
 import cn.edu360.mybatis.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import java.util.List;
 
@@ -13,9 +15,10 @@ public class OrdersServiceImpl implements OrdersService {
 
     @Override
     public List<OrdersModel> queryOrdersList(Integer ordersId) {
-        SqlSession session = MybatisUtil.getFactory().openSession();
-        OrdersDao mapper = session.getMapper(OrdersDao.class);
-        List<OrdersModel> ordersModels = mapper.queryOrdersList(ordersId);
+        SqlSessionFactory factory = MybatisUtil.getFactory();
+        SqlSession session = factory.openSession();
+        OrdersDao ordersDao=session.getMapper(OrdersDao.class);
+        List<OrdersModel> ordersModels = ordersDao.queryOrdersList(ordersId);
         for (OrdersModel ordersModel:ordersModels
              ) {
             System.out.println(ordersModel);
